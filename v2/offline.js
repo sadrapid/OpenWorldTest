69f10d3c7d68d190405a81967addc819e36d46cb
Sadrapid <Darkwep2014@yandex.ru>
2019-11-29 14:05:35 +0300

    v1 and v2 splitted

---
+++ v2/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1575025501,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"Images/grid.png",
+		"Images/framepointer-sheet0.png",
+		"Images/tilemap.png",
+		"Images/terrain.png",
+		"Images/player-sheet0.png",
+		"Images/tilemapback.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"jquery.gritter.js",
+		"jquery.gritter.css"
+	]
+}
